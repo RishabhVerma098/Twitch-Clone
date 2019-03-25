1a195dc96b8692335a0438a02f5aad97077b7c2f
rishabh verma <vermarishabh0987@gmail.com>
2019-03-25 20:53:41 +0530

    componenets made route setup

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,31 +1,21 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
-import Home from "./Home";
-
-const about = () => {
-  return (
-    <div>
-      <h1>About us</h1>
-    </div>
-  );
-};
-
-const error = () => {
-  return (
-    <div>
-      <h3>404 ERROR</h3>
-    </div>
-  );
-};
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/about/" component={about} />
-          <Route component={error} />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" component={StreamCreate} />
+          <Route path="/streams/delete" component={StreamDelete} />
+          <Route path="/streams/edit" component={StreamEdit} />
+          <Route path="/streams/show" component={StreamShow} />
         </Switch>
       </BrowserRouter>
     );
